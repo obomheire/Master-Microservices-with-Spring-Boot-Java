7897d1e87efbef1fd2a6863a4edbf767a8c0d205
obomheire <obomheire@gmail.com>
2022-05-31 07:58:02 +0100

    Added cuurency exchange

---
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurencyExchangeController.java
@@ -0,0 +1,40 @@
+package com.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurencyExchangeController {
+
+    @Autowired
+    private CurrencyExchangeRepository repository;
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        // CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+        if (currencyExchange == null) {
+            throw new RuntimeException("Unable to find currency: " + from + " to " + to);
+        }
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
+    }
+
+// @RestController
+// public class CurencyExchangeController {
+
+//     @GetMapping("/currency-exchange/from/{from}/to/{to}")
+//     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable  String to) {
+//         return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+//     }
+    
+}
