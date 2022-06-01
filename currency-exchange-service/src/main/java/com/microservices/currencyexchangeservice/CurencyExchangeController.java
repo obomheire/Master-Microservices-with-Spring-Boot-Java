4a9bcd1557aa132b95ce2424ebae01d1b22f1004
obomheire <obomheire@gmail.com>
2022-06-01 20:23:47 +0100

    Eureka added

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurencyExchangeController.java
@@ -1,6 +1,6 @@
 package com.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
+// import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
